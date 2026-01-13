bde6e131d521c2f46a6b3e54cde9ac607c378c43
Jean Tivan <jptivan53@gmail.com>
2026-01-13 19:39:24 +0100

    FIX: campo 'created_at' en tabla 'auth_providers'. ADD: Tablas para utilizar 2fa

--- db/migrations/001_init_schema.sql
+++ db/migrations/001_init_schema.sql
@@ -2,11 +2,13 @@
 -- Needed for gen_random_uuid()
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
-
 -- Enum types
+
 -- Authentications providers supported by the system
 CREATE TYPE AUTH_PROVIDER as ENUM ('local', 'google', 'github');
 
+-- 2fa types available
+CREATE TYPE TWO_FA_METHOD as ENUM('totp', 'email');
 
 -- users table: Represents core identity of a user
 CREATE TABLE IF NOT EXISTS users (
@@ -30,7 +32,7 @@ CREATE TABLE IF NOT EXISTS auth_providers (
 	provider AUTH_PROVIDER NOT NULL,
 	provider_uid VARCHAR(255) NOT NULL,
 	password_hash VARCHAR(255),
-	created_at TIMESTAMP DEFAULT NOT NULL DEFAULT NOW(),
+	created_at TIMESTAMP NOT NULL DEFAULT NOW(),
 
 	-- A provider identity can belong to only one user
 	CONSTRAINT auth_providers_unique_identity UNIQUE (provider, provider_uid)
@@ -49,6 +51,22 @@ CREATE TABLE IF NOT EXISTS sessions (
 	revoked_at TIMESTAMP
 )
 
+-- user_2fa table: To store information about 2 Factor Authentication of user
+CREATE TABLE IF NOT EXISTS user_2fa (
+	id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+	user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+	method TWO_FA_METHOD NOT NULL,
+	secret VARCHAR(255) NOT NULL,
+	enabled_at TIMESTAMP NOT NULL,
+	last_used_at TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS user_2fa_recovery_codes (
+	id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+	user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+	code_hash VARCHAR(255) NOT NULL,
+	used_at TIMESTAMP
+);
 
 -- Indexes for performance
 CREATE INDEX idx_auth_providers_user_id ON auth_providers(user_id);
@@ -56,6 +74,10 @@ CREATE INDEX idx_auth_providers_user_id ON auth_providers(user_id);
 CREATE INDEX idx_sessions_user_id ON sessions(user_id);
 
 CREATE INDEX idx_sessions_refresh_token_hash ON sessions(refresh_token_hash);
+
+CREATE INDEX idx_user_2fa_user_id ON user_2fa(user_id);
+
+CREATE INDEX idx_user_2fa_recovery_codes_user_id ON user_2fa_recovery_codes(user_id);
 
 -- Triggers for updated_at
 
