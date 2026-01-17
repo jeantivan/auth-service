950a1d1576423e9d3fb9cca7ba24e8b6bde79768
Jean Tivan <jptivan53@gmail.com>
2026-01-17 13:17:25 +0100

    CREATE: Endpoint que se utiliza para crear usuarios

---
+++ docker/backend/src/routes/auth/register.ts
@@ -0,0 +1,19 @@
+import { FastifyInstance, FastifyPluginOptions } from 'fastify';
+import { registerController } from '../../controllers/auth.controller';
+
+const schema = {
+	body: {
+		type: 'object',
+		required: ['email', 'password'],
+		properties: {
+			email: { type: 'string', format: 'email' },
+			password: { type: 'string', minLength: 8, maxLength: 72 }
+		}
+	}
+}
+export default async function (
+	fastify: FastifyInstance,
+	opts: FastifyPluginOptions
+) {
+	fastify.post('/register', { schema}, registerController);
+}
