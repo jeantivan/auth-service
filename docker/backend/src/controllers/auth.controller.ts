e0869e14192ca0c8666105575d072e9d45c5b255
Jean Tivan <jptivan53@gmail.com>
2026-01-18 13:22:34 +0100

    ADD: Controlador del login

--- docker/backend/src/controllers/auth.controller.ts
+++ docker/backend/src/controllers/auth.controller.ts
@@ -1,5 +1,5 @@
 import { FastifyReply, FastifyRequest } from "fastify";
-import { registerUser } from "../services/auth.service";
+import { registerUser, loginUser } from "../services/auth.service";
 
 export async function registerController(
 	request: FastifyRequest,
@@ -8,4 +8,18 @@ export async function registerController(
 	const user = await registerUser(request.server, request.body as any);
 
 	return reply.code(201).send(user);
+}
+
+export async function loginController(
+	request: FastifyRequest,
+	reply: FastifyReply,
+) {
+	const meta = {
+		userAgent: request.headers['user-agent'],
+		ip: request.ip
+	};
+
+	const result = await loginUser(request.server, request.body as any, meta);
+
+	return reply.send(result);
 }
