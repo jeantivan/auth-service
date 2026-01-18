b892096c51cdb6597535fc66c1f2b4a9f1df922a
Jean Tivan <jptivan53@gmail.com>
2026-01-18 23:11:59 +0100

    ADD: Logica para hacer login, crear una sesion y guardar en la base de datos un identificador de esta

--- docker/backend/src/services/auth.service.ts
+++ docker/backend/src/services/auth.service.ts
@@ -1,8 +1,11 @@
 import bcrypt from 'bcrypt';
+import crypto from 'crypto';
 import { FastifyInstance } from 'fastify';
+import { LoginInput, LoginResult } from '../domain/auth.types';
 import { RegisterInput, RegisteredUser } from '../domain/user';
 import { findUserByEmail, createUser } from '../repositories/user.repository';
-import { createLocalAuthProvider } from '../repositories/auth-provider.repository';
+import { createLocalAuthProvider, findLocalAuthProvider } from '../repositories/auth-provider.repository';
+import { createSession } from '../repositories/session.repository';
 
 export async function registerUser(
 	fastify: FastifyInstance,
@@ -40,4 +43,51 @@ export async function registerUser(
 	} finally {
 		client.release();
 	}
+}
+
+export async function loginUser(
+	fastify: FastifyInstance,
+	input: LoginInput,
+	meta: { userAgent?: string, ip?: string }
+): Promise<LoginResult> {
+	const email = input.email.toLowerCase();
+	const password = input.password;
+	const client = await fastify.pg.connect();
+
+	try {
+		const auth = await findLocalAuthProvider(client, email);
+
+		if (!auth)
+			throw fastify.httpErrors.unauthorized('Invalid email or password');
+
+		if (!auth.is_active)
+			throw fastify.httpErrors.unauthorized('User account is inactive');
+
+		const passwordOk = await bcrypt.compare(password, auth.password_hash);
+
+		if (!passwordOk)
+			throw fastify.httpErrors.unauthorized('Invalid email or password');
+
+		const accessToken = fastify.jwt.sign(
+			{ sub: auth.user_id },
+			{ expiresIn: '15m' }
+		);
+
+		const refreshToken = crypto.randomBytes(32).toString('hex');
+		const refreshTokenHash = crypto.createHash('sha256').update(refreshToken).digest('hex');
+
+		await createSession(
+			client,
+			auth.user_id,
+			refreshTokenHash,
+			meta.userAgent,
+			meta.ip
+		);
+
+		return {
+			accessToken,
+			refreshToken
+		}
+
+	} finally { client.release(); }
 }
