0fb06edea7c25f18f5089b814c296b3485eed618
Jean Tivan <jptivan53@gmail.com>
2026-01-21 23:19:05 +0100

    ADD: Schema  de las variables de entorno

--- docker/backend/src/env.schema.ts
+++ docker/backend/src/env.schema.ts
@@ -3,19 +3,19 @@ export type Env = {
 	JWT_SECRET: string,
 	JWT_EXP: string,
 	REFRESH_TOKEN_EXP: string
-	BACKEND_HOST: string
-	BACKEND_PORT: number
+	HOST: string
+	PORT: number
 }
 
 export const schema = {
 	type: 'object',
-	required: [ 'DB_URL', 'JWT_SECRET', 'JWT_EXP', 'REFRESH_TOKEN_EXP', 'BACKEND_HOST', 'BACKEND_PORT' ],
+	required: [ 'DB_URL', 'JWT_SECRET', 'JWT_EXP', 'REFRESH_TOKEN_EXP', 'HOST', 'PORT' ],
 	properties: {
 		DB_URL: { type: 'string' },
 		JWT_SECRET: { type: 'string' },
 		JWT_EXP: { type: 'string' },
 		REFRESH_TOKEN_EXP: { type: 'string' },
-		BACKEND_HOST: { type: 'string' },
-		BACKEND_PORT: { type: 'string' }
+		HOST: { type: 'string' },
+		PORT: { type: 'string' }
 	}
 }
