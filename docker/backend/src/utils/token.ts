52aa542b93589200e7c6ddb5e128982dbce9f2cc
Jean Tivan <jptivan53@gmail.com>
2026-01-21 19:54:09 +0100

    ADD: Extrae el refreshToken ya sea desde una cookie o desde el header

--- docker/backend/src/utils/token.ts
+++ docker/backend/src/utils/token.ts
@@ -1,4 +1,5 @@
 import crypto from 'crypto';
+import { FastifyRequest } from 'fastify';
 
 export function generateRefreshToken(): string {
 	return crypto.randomBytes(64).toString('hex');
@@ -6,4 +7,19 @@ export function generateRefreshToken(): string {
 
 export function hashToken(token: string) : string {
 	return crypto.createHash('sha256').update(token).digest('hex');
+}
+
+export function getRefreshToken(request: FastifyRequest): string | null {
+
+	if (request.cookies?.refreshToken) {
+		return request.cookies.refreshToken;
+	}
+
+	const authHeader = request.headers.authorization;;
+
+	if (authHeader?.startsWith('Bearer ')) {
+		return authHeader.replace('Bearer ', '');
+	}
+
+	return null;
 }
