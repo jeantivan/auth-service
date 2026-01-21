169061f0a0c25c1dd7926c4a68f2c074108696a0
Jean Tivan <jptivan53@gmail.com>
2026-01-22 00:59:52 +0100

    ADD: Cookie secret a las variables de entorno

--- docker/backend/src/env.schema.ts
+++ docker/backend/src/env.schema.ts
@@ -5,17 +5,19 @@ export type Env = {
 	REFRESH_TOKEN_EXP: string
 	HOST: string
 	PORT: number
+	COOKIE_SECRET: string
 }
 
 export const schema = {
 	type: 'object',
-	required: [ 'DB_URL', 'JWT_SECRET', 'JWT_EXP', 'REFRESH_TOKEN_EXP', 'HOST', 'PORT' ],
+	required: [ 'DB_URL', 'JWT_SECRET', 'JWT_EXP', 'REFRESH_TOKEN_EXP', 'HOST', 'PORT', 'COOKIE_SECRET' ],
 	properties: {
 		DB_URL: { type: 'string' },
 		JWT_SECRET: { type: 'string' },
 		JWT_EXP: { type: 'string' },
 		REFRESH_TOKEN_EXP: { type: 'string' },
 		HOST: { type: 'string' },
-		PORT: { type: 'string' }
+		PORT: { type: 'string' },
+		COOKIE_SECRET: { type: 'string' }
 	}
 }
