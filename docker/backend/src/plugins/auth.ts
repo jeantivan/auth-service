f814e3bde743c1f6102c5e829689f4907ec58e5d
Jean Tivan <jptivan53@gmail.com>
2026-01-22 01:00:56 +0100

    CHANGE: El middleware ahora atrapa el error y manda una respuesta mas bonita

--- docker/backend/src/plugins/auth.ts
+++ docker/backend/src/plugins/auth.ts
@@ -2,6 +2,10 @@ import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 
 export default async function(fastify: FastifyInstance) {
 	fastify.decorate('authenticate', async function (request: FastifyRequest, reply: FastifyReply) {
-		await request.jwtVerify();
+		try {
+			await request.jwtVerify();
+		} catch (err) {
+			reply.code(401).send({ error: "Unauthorized" });
+		}
 	})
 }
